717b842e3bb6671a4bb5619a119013432dbaad4a
Sofistication <sordonez93@gmail.com>
2017-03-23 18:25:37 -0400

    Add production API

--- config/environment.js
+++ config/environment.js
@@ -52,7 +52,7 @@ module.exports = function (environment) {
   if (environment === 'production') {
     ENV.rootURL = '/capstone-project';
     ENV.locationType = 'hash';
-    ENV.apiHost = ''; // TODO: link to deployed API!
+    ENV.apiHost = 'https://agile-chamber-58318.herokuapp.com/';
   }
 
   return ENV;
